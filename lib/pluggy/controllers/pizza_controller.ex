8d9861bfe878427ab110c8b35e7300cabc22c379
hanneslindberg <hannes.lindberg@elev.ga.ntig.se>
2025-09-12 09:36:50 +0200

    Duplicerade pizza controllers fixade

--- lib/pluggy/controllers/pizza_controller.ex
+++ lib/pluggy/controllers/pizza_controller.ex
@@ -1,9 +1,24 @@
 defmodule Pluggy.PizzaController do
+  require IEx
+
   alias Pluggy.Pizza
   alias Pluggy.Ingredients
+  alias Pluggy.User
 
   import Pluggy.Template, only: [render: 2]
   import Plug.Conn, only: [send_resp: 3, get_session: 2, put_session: 3]
+
+  def index(conn) do
+    session_user = conn.private.plug_session["user_id"]
+
+    current_user =
+      case session_user do
+        nil -> nil
+        _ -> User.get(session_user)
+      end
+
+    send_resp(conn, 200, render("pizzas/index", user: current_user, pizzas_grouped: Pizza.pizza_grouped()))
+  end
 
   def edit(conn, id) do
     pizza = Pizza.get(id) #with ingredients
